e54f7c259532d1206b67275789c22707ee723fe8
박성민 <psm7335@naver.com>
2022-07-18 23:40:29 +0900

    feat: User entity 추가

---
+++ prosn/src/main/java/com/ssafy/prosn/domain/User.java
@@ -0,0 +1,37 @@
+package com.ssafy.prosn.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private Integer point;
+
+    @Builder
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    /**
+     * @param point : 획득한 포인트
+     * @return : 갱신된 포인트
+     */
+    public int earnPoints(int point) {
+        return this.point + point;
+    }
+}
