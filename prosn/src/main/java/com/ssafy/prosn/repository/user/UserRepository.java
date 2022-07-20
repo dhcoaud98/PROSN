db00d3adcf4bc4341490db434dda703b5fcf6527
박성민 <psm7335@naver.com>
2022-07-20 16:17:08 +0900

    comment: 주석추가

--- prosn/src/main/java/com/ssafy/prosn/repository/user/UserRepository.java
+++ prosn/src/main/java/com/ssafy/prosn/repository/user/UserRepository.java
@@ -3,5 +3,8 @@ package com.ssafy.prosn.repository.user;
 import com.ssafy.prosn.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * created by seongmin on 2022/07/20
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 }
