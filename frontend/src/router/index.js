dba73b8b26a8f8d1697cd062508778c21f749f5d
jimin <jimin4661@gmail.com>
2022-07-22 17:50:29 +0900

    feat: navbar, signup, login design

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import SignUpView from '../views/SignUpView'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/signup', 
-    component: SignUpView
-  }
+  
 ]
 
 const router = new VueRouter({
