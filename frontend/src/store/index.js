84c1ee0b197bdff42e8d5e5444b18add71a84a47
jimin <jimin4661@gmail.com>
2022-07-22 11:31:28 +0900

    setting: store edit

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,17 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import accounts from './modules/accounts'
+import info from './modules/info'
+import note from './modules/note'
+import problem from './modules/problem'
+import search from './modules/search'
+import study from './modules/study'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+const store = new Vuex.Store({
   modules: {
+    accounts,
+    info,
+    note,
+    problem,
+    search,
+    study
   }
 })
+
+export default store
