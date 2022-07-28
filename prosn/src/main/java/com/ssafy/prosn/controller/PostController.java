515d313ee4b7d20ea4e34e6de8af925e13c3c399
seongmin <psm7335@naver.com>
2022-07-28 15:43:05 +0900

    feat: post controller 조회, 삭제, 좋아요싫어요 클릭 구현

--- prosn/src/main/java/com/ssafy/prosn/controller/PostController.java
+++ prosn/src/main/java/com/ssafy/prosn/controller/PostController.java
@@ -2,17 +2,18 @@ package com.ssafy.prosn.controller;
 
 import com.ssafy.prosn.domain.post.Post;
 import com.ssafy.prosn.dto.InformationRequestDto;
+import com.ssafy.prosn.dto.LikeDisLikeRequestDto;
+import com.ssafy.prosn.dto.PostAllResponseDto;
 import com.ssafy.prosn.dto.ProblemRequestDto;
 import com.ssafy.prosn.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * created by seongmin on 2022/07/28
@@ -34,5 +35,29 @@ public class PostController {
     public ResponseEntity<?> writeInformation(@RequestBody @Valid InformationRequestDto req) {
         Post post = postService.writeInformation(req);
         return ResponseEntity.ok(post.getId());
+    }
+
+    // 페이징 처리 추가하기
+    @GetMapping
+    public ResponseEntity<?> getAllPost() {
+        List<PostAllResponseDto> result = postService.showAllPost();
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPost(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(postService.showProblemDetail(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable(value = "id") Long id) {
+        postService.delete(id);
+        return ResponseEntity.ok(HttpEntity.EMPTY);
+    }
+
+    @PostMapping("/click")
+    public ResponseEntity<?> likeDisLikeClick(@RequestBody @Valid LikeDisLikeRequestDto req) {
+        postService.likeDislikeClick(req);
+        return ResponseEntity.ok(HttpEntity.EMPTY);
     }
 }
