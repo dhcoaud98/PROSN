72dc45411167e439865e146caf84e281afad7141
dhcoaud <dhcoaud98@gmail.com>
2022-07-29 11:37:52 +0900

    feature: 문제풀이현황 골격

--- frontend/src/components/Profile/SolvedProblemList.vue
+++ frontend/src/components/Profile/SolvedProblemList.vue
@@ -1,10 +1,18 @@
 <template>
-  <div></div>
+  <v-container>
+    <!-- v-for 사용하기 -->
+    <solved-problem-list-items></solved-problem-list-items>
+  </v-container>
 </template>
 
 <script>
-export default {
+import SolvedProblemListItems from './SolvedProblemListItems.vue'
 
+export default {
+  name: 'SolvedProblemList',
+  components: {
+    SolvedProblemListItems,
+  }
 }
 </script>
 
