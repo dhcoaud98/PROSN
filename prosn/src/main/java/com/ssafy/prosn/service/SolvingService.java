6f8e535e62e3ca37a52bc7224662283c4080e47b
고유라 <koyura183982@naver.com>
2022-08-02 13:34:25 +0900

    feat: 문제풀이현황 서비스 추가

--- prosn/src/main/java/com/ssafy/prosn/service/SolvingService.java
+++ prosn/src/main/java/com/ssafy/prosn/service/SolvingService.java
@@ -1,2 +1,15 @@
-package com.ssafy.prosn.service;public class SolvingService {
+package com.ssafy.prosn.service;
+
+import com.ssafy.prosn.domain.profile.status.Solving;
+import com.ssafy.prosn.dto.SolvingRequestDto;
+import com.ssafy.prosn.dto.SolvingResponseDto;
+
+import java.util.List;
+
+/**
+ * created by yura on 2022/08/01
+ */
+public interface SolvingService {
+    Solving saveSolving(SolvingRequestDto solvingRequestDto);
+    List<SolvingResponseDto> showAllSolving();
 }
