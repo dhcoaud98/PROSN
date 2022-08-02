5a194cc41680df639a51897ba2d77ccd40da689f
고유라 <koyura183982@naver.com>
2022-08-02 17:38:15 +0900

    feat: Swagger api 추가

--- prosn/src/main/java/com/ssafy/prosn/controller/SolvingController.java
+++ prosn/src/main/java/com/ssafy/prosn/controller/SolvingController.java
@@ -5,6 +5,7 @@ import com.ssafy.prosn.dto.SolvingResponseDto;
 import com.ssafy.prosn.service.SolvingService;
 import com.ssafy.prosn.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +27,8 @@ public class SolvingController {
     /**
      * 문제풀이 현황을 DB에 저장하는 메서드
      */
-    @PostMapping
+    @PostMapping("/save")
+    @ApiOperation(value = "문제풀이 여부 저장", notes = "문제풀이 여부를 문자열로 DB에 저장한다.")
     public ResponseEntity<?> saveRight(@RequestBody SolvingRequestDto req) {
         return ResponseEntity.ok(solvingService.saveSolving(req));
     }
@@ -34,7 +36,8 @@ public class SolvingController {
     /**
      * 유저 아이디에 맞는 문제풀이 현황을 불러오는 메서드
      */
-    @GetMapping
+    @GetMapping("/getSolving")
+    @ApiOperation(value = "풀이한 문제 불러오기", notes = "풀이된 문제를 불러온다.")
     public ResponseEntity<?> getAllSolving() {
         List<SolvingResponseDto> result = solvingService.showAllSolving();
         return ResponseEntity.ok(result);
