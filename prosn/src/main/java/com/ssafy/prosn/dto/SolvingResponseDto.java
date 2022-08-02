b2a4ebc1f09fb66cb112dac84cf6f293ae92fef5
고유라 <koyura183982@naver.com>
2022-08-02 13:32:32 +0900

    feat: 문제풀이현황 response dto 추가

--- prosn/src/main/java/com/ssafy/prosn/dto/SolvingResponseDto.java
+++ prosn/src/main/java/com/ssafy/prosn/dto/SolvingResponseDto.java
@@ -1,2 +1,22 @@
-package com.ssafy.prosn.dto;public class SolvingResponseDto {
+package com.ssafy.prosn.dto;
+
+import com.ssafy.prosn.domain.user.LocalUser;
+import com.ssafy.prosn.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * created by yura on 2022/08/01
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class SolvingResponseDto {
+    private Long id;
+    private Long userId;
+    private Long postId;
+    private String isRight;
+
 }
