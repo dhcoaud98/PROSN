79eb8808060c1877ae2a54d8b9d1b79aece39bff
고유라 <koyura183982@naver.com>
2022-08-02 17:38:47 +0900

    feat: Swagger api 추가

--- prosn/src/main/java/com/ssafy/prosn/domain/profile/status/Solving.java
+++ prosn/src/main/java/com/ssafy/prosn/domain/profile/status/Solving.java
@@ -4,6 +4,7 @@ import com.ssafy.prosn.converter.BooleanToYNConverter;
 import com.ssafy.prosn.domain.BaseEntity;
 import com.ssafy.prosn.domain.post.Problem;
 import com.ssafy.prosn.domain.user.User;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,17 +22,21 @@ public class Solving extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(example = "문제 id")
     private Long id;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @ApiModelProperty(example = "사용자 id")
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "post_id")
+    @ApiModelProperty(example = "게시글 id")
     private Problem problem;
 
     @Convert(converter = BooleanToYNConverter.class)
+    @ApiModelProperty(example = "풀이 여부")
     private String isRight;
 
     @Builder
