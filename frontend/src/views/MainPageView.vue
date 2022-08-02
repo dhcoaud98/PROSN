c107e24feba6cda59ef81ab0e1187e54b4f75130
namseongeun <myaeja200@naver.com>
2022-08-02 11:59:05 +0900

    feature: MainPageView.vue 통일

--- frontend/src/views/MainPageView.vue
+++ frontend/src/views/MainPageView.vue
@@ -1,54 +1,37 @@
 <template>
-  <v-container fluid class="mt-2 rounded-lg create-something" >
-    <!-- 정보게시하기 -->
+  <v-container fluid class="mt-2 white">
+    <!-- row 1: 메인 피드와 sidebar 모두를 감싸는 줄 -->
     <v-row>
-      <v-col cols="12" class="ma-3">
-        <h2 class="px-3 font-weight-regular">정보 게시하기</h2>
+
+      <!-- col 1: 메인 피드 부분 -->
+      <v-col cols="12" md="8">
+        <!-- row 1-1: 상단 탭; 문제/문제집, 정보 -->
+        <v-row class="bottom-line justify-center mt-5 mx-5">
+          <v-col xl="4" lg="6" md="6" sm="6" class="tab-hover">
+            <p class="text-center mb-0 font-weight-bold text-grey">문제/문제집</p>
+          </v-col>
+          <v-col xl="4" lg="6" md="6" sm="6" class="tab-hover">
+            <p class="text-center mb-0 font-weight-bold text-grey">정보</p>
+          </v-col>
+        </v-row>
+
+        <!-- row 1-2: 피드 컨텐츠 부분 -->
+        <v-row>
+          <!-- 메인 피드 1. -- 문제/문제집 -->
+          <recent-problem></recent-problem>
+
+          <!-- 메인 피드 2. -- 정보 -->
+            <info></info>
+        </v-row>
+      </v-col>
+
+      <!-- col 2: 사이드 바 -->
+      <v-col md="4" class="d-md-flex d-md-none grey lighten-4">
+        <side-bar></side-bar>
       </v-col>
     </v-row>
-
-    <v-divider class="mx-5" color="#A384FF"></v-divider>
-
-    <!-- INFO글 정보 작성란 -->
-      <v-form class="px-5">
-        <!-- 카테고리 -->
-        <v-row class="mx-2 mt-5">
-          <v-col class="col-12 pa-0 mb-2"><p class="mb-0 font-weight-bold">카테고리</p></v-col>
-          <v-col class="col-12 pa-0">
-            <!-- 이거 다시 따져볼 필요성이 있음 -->
-            <v-autocomplete :items="categoriesItems" label="주제를 선택하세요 (복수선택 가능)" required dense chips small-chips multiple></v-autocomplete>          
-          </v-col>
-        </v-row>
-
-        <!-- 게시글제목 -->
-        <v-row class="mx-2 mt-5">
-          <v-col class="col-12 pa-0 mb-2"><p class="font-weight-bold mb-0">제목</p></v-col>
-          <v-col class="col-12 pa-0">
-            <v-text-field label="글 제목을 입력하세요 (최대 20자)" maxlength="20" counter required dense></v-text-field>
-          </v-col>
-        </v-row>
-
-        <!-- 지문 -->
-        <v-row class="mx-2 mt-5 mb-2">
-          <v-col class="col-12 pa-0 mb-2"><p class="font-weight-bold mb-0">지문</p></v-col>
-          <v-col class="col-12 pa-0">
-            <v-textarea label="글 내용을 입력하세요" rows="15" counter required dense></v-textarea>
-          </v-col>
-        </v-row>
-
-        <v-divider color="#A384FF"></v-divider>
-        
-        <!-- 게시버튼 -->
-        <v-row class="my-2 ">
-          <v-col cols="12" class="d-flex justify-end pa-0">
-            <!-- router - 1  -->
-            <v-btn large rounded color="#EA4C89" class="white--text font-weight-bold me-5 mt-2 py-5">취소하기</v-btn>
-            <!-- submit -->
-            <v-btn large rounded type="submit" color="#A384FF" class="white--text font-weight-bold me-3 mt-2 py-5">게시하기</v-btn>
-          </v-col>
-        </v-row>
-      </v-form>
-  </v-container> 
+  </v-container>
+      
 </template>
 
 <script>
@@ -89,20 +72,5 @@ export default {
   }
   .w-100 {
     width: 100%;
-  }
-</style>
-
-<style>
-  .v-application--wrap {
-  max-width: none;
-  }
-  .container {
-    max-width: none;
-  }
-  .w-100 {
-    width: 100%;
-  }
-  .create-something {
-    background-color: #EDE7F6;
   }
 </style>
