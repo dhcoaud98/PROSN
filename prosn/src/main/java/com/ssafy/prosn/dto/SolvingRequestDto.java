e1a909ce3c210faf80fcc70fcecea1aa80c6069d
고유라 <koyura183982@naver.com>
2022-08-02 13:32:06 +0900

    feat: 문제풀이현황 request dto 추가

--- prosn/src/main/java/com/ssafy/prosn/dto/SolvingRequestDto.java
+++ prosn/src/main/java/com/ssafy/prosn/dto/SolvingRequestDto.java
@@ -1,2 +1,17 @@
-package com.ssafy.prosn.dto;public class SolvingRequestDto {
+package com.ssafy.prosn.dto;
+
+import lombok.*;
+
+/**
+ * created by yura on 2022/08/01
+ */
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SolvingRequestDto {
+    private Long id;
+    private Long userId;
+    private Long postId;
+    private String isRight;
 }
