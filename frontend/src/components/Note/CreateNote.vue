86d4d5e85ea1e5170f1afc00102c99d5c215f167
jimin <jimin4661@gmail.com>
2022-08-04 17:34:00 +0900

    feat: create createnote

--- frontend/src/components/Note/CreateNote.vue
+++ frontend/src/components/Note/CreateNote.vue
@@ -2,20 +2,17 @@
   <!-- 전체적인 틀 -->
   <v-container fluid class="white">
     <!-- 문제 정보 -->
-    <v-row class="align-center">
+    <v-row class="align-center ml-5 mt-5 color-FAF0F3">
       <v-col cols="1"><v-icon large color="black darken-2">mdi-group</v-icon></v-col>
         <v-col class="pb-0">
           <v-row>
             <span class="category-tag text-center pa-1 mr-2 font-parent-xsml">#네트워크</span>
             <span class="category-tag text-center pa-1 mr-2 font-parent-xsml">#알고리즘</span>
-          </v-row>
-          <v-row>
             <span class="font-parent-mid-l font-weight-bold mr-2">HTTP 포트</span>
             <p>(
               <span class="red--text font-weight-bold">5</span>/<span class="font-weight-bold">10</span>
             )</p>
           </v-row>
-          
         </v-col>
     </v-row>
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import accounts from './modules/accounts.js'
-import info from './modules/info.js'
-import note from './modules/note.js'
-import problem from './modules/problem.js'
-import search from './modules/search.js'
-import study from './modules/study.js'
+import accounts from '@/store/modules/accounts.js'
+import info from '@/store/modules/info.js'
+import note from '@/store/modules/note.js'
+import problem from '@/store/modules/problem.js'
+import search from '@/store/modules/search.js'
+import study from '@/store/modules/study.js'
 
 Vue.use(Vuex)
 
