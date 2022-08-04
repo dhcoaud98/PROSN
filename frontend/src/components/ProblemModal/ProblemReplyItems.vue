022f1855d597c664a71485caa4795330c79c758d
namseongeun <myaeja200@naver.com>
2022-08-05 01:17:29 +0900

    feat: infoModal 에서 댓글 다 잇기

--- frontend/src/components/ProblemModal/ProblemReplyItems.vue
+++ frontend/src/components/ProblemModal/ProblemReplyItems.vue
@@ -9,7 +9,6 @@
         <v-col cols="4" class="d-flex align-end secondary--text text--lighten-3 pa-0">작성자 |</v-col>
         <v-col cols="5" class="d-flex align-end secondary--text text--lighten-3 pa-0">최종작성일시 |</v-col>
         <v-col cols="3" class="d-flex justify-end py-0 font-weight-bold #817EF4--text" style="color:#817EF4"><h3>답글</h3></v-col>
-        <!-- 2022.08.03. 답글 누르면 답글 입력란 보이게 하기 -->
       </v-row>
     </v-container>
   </v-card>
