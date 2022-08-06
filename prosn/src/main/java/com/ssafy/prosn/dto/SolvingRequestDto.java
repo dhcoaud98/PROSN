2b53197a61ed19f32203a7c1c9ca017b58084502
박성민 <psm7335@naver.com>
2022-08-06 16:01:26 +0900

    feat: 문제풀이 컨트롤러 추가

--- prosn/src/main/java/com/ssafy/prosn/dto/SolvingRequestDto.java
+++ prosn/src/main/java/com/ssafy/prosn/dto/SolvingRequestDto.java
@@ -2,6 +2,8 @@ package com.ssafy.prosn.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * created by yura on 2022/08/01
  * updated by seongmin on 2022/08/06
@@ -11,6 +13,8 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SolvingRequestDto {
+    @NotNull(message = "문제는 필수 입력값입니다.")
     private Long pid;
+    @NotNull(message = "풀이 결과는 필수 입력값입니다.")
     private boolean right; // IsRight 로 하면 매핑 안됨.
 }
