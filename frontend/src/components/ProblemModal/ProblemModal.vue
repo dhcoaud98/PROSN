9e206e091d8a81bdcb9108deca959da1bfe5ac8e
namseongeun <myaeja200@naver.com>
2022-08-06 16:24:17 +0900

    feat: modal re-size

--- frontend/src/components/ProblemModal/ProblemModal.vue
+++ frontend/src/components/ProblemModal/ProblemModal.vue
@@ -1,98 +1,104 @@
 <template>
   <!-- 2022.07.26 문제 모달 (남성은) -->
   <!-- 2022.08.03 댓글보기 버튼 활성화 (남성은) -->
-    <v-container class="rounded-lg">
-      <v-row class="d-flex justify-center">
-        <!-- 문제부분 (항상 떠있음) -->
-        <v-col cols="6" width="600" class="pa-0 info-modal-white">              
-          <v-card-text class="d-flex justify-space-between">
-            <v-btn @click="event()" text class="font-weight-bold">크게보기</v-btn>
-            <v-btn @click="goBack()" text class="font-weight-bold">뒤로가기</v-btn>
-          </v-card-text>
-  
-          <!-- 문제 제목 -->
-          <!-- {{ problem.pk }}. {{ problem.MAIN_TEXT}} -->
-          <v-card-title class="font-weight-bold black--text">
-            <h2>158. 정보 표현의 기본 장치</h2> 
-          </v-card-title>
-  
-          <!-- 문제 본문 -->
-          <v-card-text>
-            <!-- 카테고리 라벨 -->
-            <!-- for문으로 돌리면 될듯 -->
-            <div>
-              <v-chip small color="#A384FF" class="white--text">http</v-chip>
-            </div>
-          </v-card-text>
-  
-          <!-- 문제 -->
-          <v-card-text class="font-weight-bold mb-2">
-            <h2 class="black--text">
-            다음 설명에 맞는 장치로 적절한 것은 무엇인가요?
-            </h2>
-            <!-- 문제지문 -->
-            <h3 class="black--text my-4">
-              컴퓨터에는 (       )라고 불리는 굉장히 많은 스위치가 있고,<br>
-              on/off 상태를 통해 0과 1을 표현합니다.
-            </h3>
-            <div>
-              <v-container>
-                <!-- 문제보기: 이것도 랜덤으로 for문 돌리기/ 체크박스로 라디오 묶는거....ㅠ-->
-                <v-form>
-                  <v-row>
-                    <v-radio-group column class="ms-5">
-                      <v-radio label="보기1" color="info" value="보기1"></v-radio>
-                      <v-radio label="보기2" color="info" value="보기2"></v-radio>
-                      <v-radio label="보기3" color="info" value="보기3"></v-radio>
-                      <v-radio label="보기4" color="info" value="보기4"></v-radio>
-                    </v-radio-group>
-                  </v-row>
-                  <!-- 저작권 / 버튼 -->
-                  <v-row class="d-flex justify-space-between">
-                    <!-- 출제자 정보 -->
-                    <v-col>
-                      <div>출제자 | </div>
-                      
-                      <div>출제일 | </div>
-                    </v-col>
-
-                    <!-- 버튼 그룹 if로 자기 문제인 경우랑 아닌 경우 나눠서 보여주기 -->
-                    <v-col class="px-0">
-                      <!-- 좋아요 버튼 -->
-                      <v-btn class="ms-2" icon color="blue lighten-2">
-                        <v-icon>mdi-thumb-up</v-icon>
-                      </v-btn>
-                      <!-- 싫어요 버튼 -->
-                      <v-btn class="ms-2" icon color="red lighten-2">
-                        <v-icon>mdi-thumb-down</v-icon>
-                      </v-btn>
-                      <!-- 스크랩 버튼 -->
-                      <v-btn class="ms-2" icon color="dark lighten-2">
-                        <v-icon>mdi-folder-open</v-icon>
-                      </v-btn>                    
-                      <!-- 제출 버튼 -->
-                      <v-btn rounded class="ms-2" color="#F3F3F4">제출</v-btn>
-                    </v-col>
-                  </v-row>
-                </v-form>
-              </v-container>
-              <hr>
-              <!-- 댓글보기 -->
-              <v-btn @click="showReplies" id="show-replies-btn" class="font-weight-bold mt-2" text>댓글보기</v-btn>
-              <v-btn @click="noShowReplies" id="no-show-replies-btn" class="d-none font-weight-bold mt-2" text>댓글접기</v-btn>
-            </div>            
-          </v-card-text>
-        </v-col>
-
-        <!-- 평소에는 안보이는 세로선 -->
-        <v-divider id="show-divider" class="my-5 d-none" vertical></v-divider>
-
-        <!-- 평소에는 안보이는 댓글창 -->
-        <v-col cols="6" width="600" id="show-replies" class="pa-0 d-none">
-          <problem-reply></problem-reply>
-        </v-col>
-      </v-row>
+  <v-container class="modal" appear>
+    <v-container class="modal modal-overlay" @click.self="$emit('close')">
+      <v-container class="modal-window">
+        <v-container class="rounded-lg modal-content">
+          <v-row class="d-flex justify-center">
+            <!-- 문제부분 (항상 떠있음) -->
+            <v-col width="600" class="pa-0 info-modal-white">              
+              <v-card-text class="d-flex justify-space-between">
+                <v-btn @click="event()" text class="font-weight-bold">크게보기</v-btn>
+                <v-btn @click="goBack()" text class="font-weight-bold">뒤로가기</v-btn>
+              </v-card-text>
+      
+              <!-- 문제 제목 -->
+              <!-- {{ problem.pk }}. {{ problem.MAIN_TEXT}} -->
+              <v-card-title class="font-weight-bold black--text">
+                <h2>158. 정보 표현의 기본 장치</h2> 
+              </v-card-title>
+      
+              <!-- 문제 본문 -->
+              <v-card-text>
+                <!-- 카테고리 라벨 -->
+                <!-- for문으로 돌리면 될듯 -->
+                <div>
+                  <v-chip small color="#A384FF" class="white--text">http</v-chip>
+                </div>
+              </v-card-text>
+      
+              <!-- 문제 -->
+              <v-card-text class="font-weight-bold mb-2">
+                <h2 class="black--text">
+                다음 설명에 맞는 장치로 적절한 것은 무엇인가요?
+                </h2>
+                <!-- 문제지문 -->
+                <h3 class="black--text my-4">
+                  컴퓨터에는 (       )라고 불리는 굉장히 많은 스위치가 있고,<br>
+                  on/off 상태를 통해 0과 1을 표현합니다.
+                </h3>
+                <div>
+                  <v-container>
+                    <!-- 문제보기: 이것도 랜덤으로 for문 돌리기/ 체크박스로 라디오 묶는거....ㅠ-->
+                    <v-form>
+                      <v-row>
+                        <v-radio-group column class="ms-5">
+                          <v-radio label="보기1" color="info" value="보기1"></v-radio>
+                          <v-radio label="보기2" color="info" value="보기2"></v-radio>
+                          <v-radio label="보기3" color="info" value="보기3"></v-radio>
+                          <v-radio label="보기4" color="info" value="보기4"></v-radio>
+                        </v-radio-group>
+                      </v-row>
+                      <!-- 저작권 / 버튼 -->
+                      <v-row class="d-flex justify-space-between">
+                        <!-- 출제자 정보 -->
+                        <v-col>
+                          <div>출제자 | </div>
+                          
+                          <div>출제일 | </div>
+                        </v-col>
+    
+                        <!-- 버튼 그룹 if로 자기 문제인 경우랑 아닌 경우 나눠서 보여주기 -->
+                        <v-col class="px-0">
+                          <!-- 좋아요 버튼 -->
+                          <v-btn class="ms-2" icon color="blue lighten-2" @click="changeLikeStatus" id="upIcon">
+                            <v-icon>{{upText}}</v-icon>
+                          </v-btn>
+                          <!-- 싫어요 버튼 -->
+                          <v-btn class="ms-2" icon color="red lighten-2" @click="changeHateStatus" id="downIcon">
+                            <v-icon>{{downText}}</v-icon>
+                          </v-btn>
+                          <!-- 스크랩 버튼 -->
+                          <v-btn class="ms-2" icon color="dark lighten-2" @click="changeScrapStatus" id="scrapIcon">
+                            <v-icon>{{scrapText}}</v-icon>
+                          </v-btn>                    
+                          <!-- 제출 버튼 -->
+                          <v-btn rounded class="ms-2" color="#F3F3F4">제출</v-btn>
+                        </v-col>
+                      </v-row>
+                    </v-form>
+                  </v-container>
+                  <hr>
+                  <!-- 댓글보기 -->
+                  <v-btn @click="showReplies" id="show-replies-btn" class="font-weight-bold mt-2" text>댓글보기</v-btn>
+                  <v-btn @click="noShowReplies" id="no-show-replies-btn" class="d-none font-weight-bold mt-2" text>댓글접기</v-btn>
+                </div>            
+              </v-card-text>
+            </v-col>
+    
+            <!-- 평소에는 안보이는 세로선 -->
+            <v-divider id="show-divider" class="my-5 d-none" vertical></v-divider>
+    
+            <!-- 평소에는 안보이는 댓글창 -->
+            <v-col width="600" id="show-replies" class="pa-0 d-none">
+              <problem-reply></problem-reply>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-container>
     </v-container>
+  </v-container>
 
 </template>
 
@@ -104,7 +110,54 @@ export default {
   components: {
     ProblemReply
   },
+  data () {
+    return {
+      upText: 'thumb_up_off_alt',
+      downText: 'thumb_down_off_alt',
+      scrapText: 'bookmark_border',
+    }
+  },
   methods: {
+    changeLikeStatus() {
+        /* 
+        버튼 클릭하면 색이 바뀌도록
+        thumb up --> thumb up off alt
+        thumb down --> thumb down off alt
+        bookmark border --> bookmark
+        */
+        /* 싫어요가 눌려 있는 상태에서 좋아요를 누르면 싫어요가 취소되는 것도 추가 */
+
+        if (this.upText === "thumb_up_off_alt") {
+          // 좋아요를 눌러야 하는데 이미 싫어요가 눌려져 있는 상태
+          if (this.downText === "thumb_down") {
+              // console.log(this.downText)
+              this.downText = "thumb_down_off_alt"
+          }
+          this.upText = "thumb_up"
+          } else {
+            this.upText = "thumb_up_off_alt"
+          }
+    },
+    changeHateStatus() {
+        /* 좋아요가 눌려 있는 상태에서 싫어요를 누르면 좋아요가 취소되는 것도 추가 */
+        if (this.downText === "thumb_down_off_alt") {
+            this.downText = "thumb_down"
+            // 싫어요를 눌렀는데 이미 좋아요가 눌러져 있는 상태
+            if (this.upText === "thumb_up") {
+                this.upText = "thumb_up_off_alt"
+            }
+       } else {
+            this.downText = "thumb_down_off_alt"
+       }
+    },
+    changeScrapStatus() {
+       if (this.scrapText === "bookmark_border") {
+            this.scrapText = "bookmark"
+       } else {
+            this.scrapText = "bookmark_border"
+       }
+    },
+    
     // 2022.08.03. 댓글보기 버튼 누를 때
     showReplies: function (event) {
       const showRepliesBtn = document.querySelector("#show-replies-btn")
@@ -143,5 +196,59 @@ export default {
 <style>
 .info-modal-white {
   background-color: white;
+}
+</style>
+
+<style lang="stylus" scoped>
+.modal {
+  &.modal-overlay {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    z-index: 30;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+  }
+
+  &-window {
+    background: #F3F1F5;
+    border-radius: 4px;
+    overflow: hidden;
+    width: auto;
+  }
+
+  &-content {
+    padding: 10px;
+    width: 100%;
+    height: 100%
+  }
+}
+
+// 오버레이 트랜지션
+.modal-enter-active, .modal-leave-active {
+  transition: opacity 0.4s;
+
+  // 오버레이에 포함되어 있는 모달 윈도의 트랜지션
+  .modal-window {
+    transition: opacity 0.4s, transform 0.4s;
+  }
+}
+
+// 딜레이가 적용된 모달 윈도가 제거된 후에 오버레이가 사라짐
+.modal-leave-active {
+  transition: opacity 0.6s ease 0.4s;
+}
+
+.modal-enter, .modal-leave-to {
+  opacity: 0;
+
+  .modal-window {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
 }
 </style>
