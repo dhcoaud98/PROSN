b4aa716de6542e28df5785b61ae040d4e57d65a0
박성민 <psm7335@naver.com>
2022-08-10 20:42:43 +0900

    fix: 오답노트 수정

--- prosn/src/main/java/com/ssafy/prosn/controller/WrongAnswerController.java
+++ prosn/src/main/java/com/ssafy/prosn/controller/WrongAnswerController.java
@@ -15,9 +15,11 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.*;
+
 /**
  * created by seongmin on 2022/08/01
- * updated by seongmin on 2022/08/07
+ * updated by seongmin on 2022/08/10
  */
 @RestController
 @RequiredArgsConstructor
@@ -32,39 +34,39 @@ public class WrongAnswerController {
     public ResponseEntity<?> save(@RequestBody @Valid WrongAnswerRequestDto req) {
         WrongAnswer result = wrongAnswerService.save(req, userService.getMyInfoBySecret().getId());
         log.info("result = {}", result);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(CREATED).build();
     }
 
     @PatchMapping
     public ResponseEntity<?> write(@RequestBody @Valid WrongNoteRequestDto req) {
         wrongAnswerService.write(req, userService.getMyInfoBySecret().getId());
-        return ResponseEntity.ok(HttpEntity.EMPTY);
+        return ResponseEntity.status(CREATED).build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         wrongAnswerService.delete(id, userService.getMyInfoBySecret().getId());
-        return ResponseEntity.ok(HttpEntity.EMPTY);
+        return ResponseEntity.status(NO_CONTENT).build();
     }
 
     // 오답노트 전체 조회
     @GetMapping("/all")
-    public ResponseEntity<?> getNote(@RequestParam Pageable pageable, boolean isWrite) {
+    public ResponseEntity<?> getNote(Pageable pageable, @RequestParam boolean isWrite) {
         NoteResponseDto result = wrongAnswerService.getNote(userService.getMyInfoBySecret().getId(), pageable, isWrite);
-        return ResponseEntity.status(HttpStatus.OK).body(result);
+        return ResponseEntity.status(OK).body(result);
     }
 
     @GetMapping("/tag")
-    public ResponseEntity<?> getNoteByTag(@RequestParam Pageable pageable, boolean isWrite, String tag) {
+    public ResponseEntity<?> getNoteByTag(Pageable pageable, @RequestParam boolean isWrite, @RequestParam String tag) {
         NoteResponseDto result = wrongAnswerService.getNoteByTag(userService.getMyInfoBySecret().getId(), pageable, isWrite, tag);
-        return ResponseEntity.status(HttpStatus.OK).body(result);
+        return ResponseEntity.status(OK).body(result);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getNoteDetail(@PathVariable(value = "id") Long id) {
         WrongAnswerNoteDetailResponseDto result = wrongAnswerService.getNoteDetail(id, userService.getMyInfoBySecret().getId());
         log.info("result = {}", result);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(OK).body(result);
     }
 
 }
