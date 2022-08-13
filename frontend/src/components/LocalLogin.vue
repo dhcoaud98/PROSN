8385e45d64c697ff566191ec30de1b6f217b3a4f
박성민 <psm7335@naver.com>
2022-08-13 14:20:46 +0900

    feat: 리프레시토큰으로 토큰 재발급

--- frontend/src/components/LocalLogin.vue
+++ frontend/src/components/LocalLogin.vue
@@ -1,56 +1,79 @@
 <template>
-  <div>
-    <!-- 2022.07.25. 회원 로그인정보 입력란 (남성은) -->
-    <!-- 0805 임지민
+	<div>
+		<!-- 2022.07.25. 회원 로그인정보 입력란 (남성은) -->
+		<!-- 0805 임지민
       - 아직 계정이 없으신가요? 와 아이디/비번 찾기 사이에 간격 늘리기
       - 회원가입 hover시 보라색 text와 보라색 underline 뜨도록 수정
       - 회원가입 글자 bold로 바꿈
      -->
-    <v-container class="my-5">
-      <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="login" class="ma-0 pa-0">
-        <v-row no-gutters>
-          <v-col cols="9" class="ma-0 pe-3">
-            <!-- 네임룰 추후 설정 필요 -->
-            <!-- ID 입력란 -->
-            <v-text-field v-model="credentials.userId" label="ID" required></v-text-field>
-            <!-- PASSWORD 입력란 -->
-            <!-- <v-text-field v-model="password" label="PASSWORD" required></v-text-field> -->
-            <v-text-field
-            v-model="credentials.password"
-            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-            :rules="[rules.required, rules.min]"
-            :type="show ? 'text' : 'password'"
-            name="input-10-2"
-            label="PASSWORD"
-            hint="At least 8 characters"
-            value=""
-            class="input-group--focused"
-            @click:append="show = !show"
-            ></v-text-field>
-          </v-col>
+		<v-container class="my-5">
+			<v-form
+				ref="form"
+				v-model="valid"
+				lazy-validation
+				@submit.prevent="login"
+				class="ma-0 pa-0"
+			>
+				<v-row no-gutters>
+					<v-col cols="9" class="ma-0 pe-3">
+						<!-- 네임룰 추후 설정 필요 -->
+						<!-- ID 입력란 -->
+						<v-text-field
+							v-model="credentials.userId"
+							label="ID"
+							required
+						></v-text-field>
+						<!-- PASSWORD 입력란 -->
+						<!-- <v-text-field v-model="password" label="PASSWORD" required></v-text-field> -->
+						<v-text-field
+							v-model="credentials.password"
+							:append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+							:rules="[rules.required, rules.min]"
+							:type="show ? 'text' : 'password'"
+							name="input-10-2"
+							label="PASSWORD"
+							hint="At least 8 characters"
+							value=""
+							class="input-group--focused"
+							@click:append="show = !show"
+						></v-text-field>
+					</v-col>
 
-          <!-- 로그인 버튼 -->
-          <v-col cols="3">
-            <v-btn type="submit" color="#A384FF" class="white--text my-0 mx-5 py-5" height="100%" width="100%">로그인</v-btn>
-          </v-col>
-        </v-row>
-      </v-form>
+					<!-- 로그인 버튼 -->
+					<v-col cols="3">
+						<v-btn
+							type="submit"
+							color="#A384FF"
+							class="white--text my-0 mx-5 py-5"
+							height="100%"
+							width="100%"
+							>로그인</v-btn
+						>
+					</v-col>
+				</v-row>
+			</v-form>
 
-      <!-- 회원가입/아이디찾기/비밀번호찾기 --> 
-      <v-row class="mt-5">
-        <v-col cols="12" class=" d-flex justify-center pa-0">
-          <p>아직 계정이 없으신가요?<router-link to="/signup" class="hover-login ml-3 font-weight-bold black--text">회원가입</router-link></p>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12" class="d-flex justify-space-around pa-0">
-          <a href="" class="black--text">아이디 찾기</a>
-          <p>|</p>
-          <a href="" class="black--text">비밀번호 찾기</a>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+			<!-- 회원가입/아이디찾기/비밀번호찾기 -->
+			<v-row class="mt-5">
+				<v-col cols="12" class="d-flex justify-center pa-0">
+					<p>
+						아직 계정이 없으신가요?<router-link
+							to="/signup"
+							class="hover-login ml-3 font-weight-bold black--text"
+							>회원가입</router-link
+						>
+					</p>
+				</v-col>
+			</v-row>
+			<v-row>
+				<v-col cols="12" class="d-flex justify-space-around pa-0">
+					<a href="" class="black--text">아이디 찾기</a>
+					<p>|</p>
+					<a href="" class="black--text">비밀번호 찾기</a>
+				</v-col>
+			</v-row>
+		</v-container>
+	</div>
 </template>
 
 <script>
@@ -85,71 +108,76 @@
 //     },
 //   }
 
-import axios from 'axios'
-import drf from '@/api/drf'
-import {mapState, mapActions } from 'vuex'
-  const accountStore = "accountStore"
+import axios from 'axios';
+import drf from '@/api/drf';
+import { mapState, mapActions } from 'vuex';
+const accountStore = 'accountStore';
 
-  export default {
-    // 2022.07.25. 아이디 (남성은)
-    data: () => ({
-      valid: true,
-      credentials : {
-        userId: '',
-        password: ''
-      },
-      show: false,
-      rules: {
-        required: value => !!value || 'Required.',
-        min: v => v.length >= 8 || 'Min 8 characters',
-      },
-    }),
+export default {
+	// 2022.07.25. 아이디 (남성은)
+	data: () => ({
+		valid: true,
+		credentials: {
+			userId: '',
+			password: '',
+		},
+		show: false,
+		rules: {
+			required: (value) => !!value || 'Required.',
+			min: (v) => v.length >= 8 || 'Min 8 characters',
+		},
+	}),
 
-    // 2022.07.25. 로그인 버튼 (남성은)
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      // ...mapActions(['login'])
-      login() {
+	// 2022.07.25. 로그인 버튼 (남성은)
+	methods: {
+		validate() {
+			this.$refs.form.validate();
+		},
+		// ...mapActions(['login'])
+		login() {
+			// axios.post(drf.accounts.login())
+			// .then(({res}) => {
+			//   console.log(res)
+			// })
+			axios({
+				url: drf.accounts.login(),
+				method: 'post',
+				data: this.credentials,
+			})
+				.then((res) => {
+					console.log('res = ', res);
+					console.log('accessToken = ', res.data.accessToken);
+					console.log('refreshToken = ', res.data.refreshToken);
+					console.log('expire : ', res.data.tokenExpiresIn);
+					let grantType = res.data.grantType.replace(
+						res.data.grantType.charAt(0),
+						res.data.grantType.charAt(0).toUpperCase()
+					);
+					console.log('grantType:', grantType);
+					this.$store.dispatch(
+						'saveToken',
+						grantType + ' ' + res.data.accessToken
+					);
+					this.$store.dispatch('saveRefreshToken', res.data.refreshToken);
+					this.$store.dispatch('saveExpiresIn', res.data.tokenExpiresIn);
 
-        // axios.post(drf.accounts.login())
-        // .then(({res}) => {
-        //   console.log(res)
-        // })
-        axios({
-                url: drf.accounts.login(),
-                method: 'post',
-                data: this.credentials
-            })
-            .then(res => {
-                console.log("res = ",res);
-                console.log("accessToken = ",res.data.accessToken);
-                console.log("refreshToken = ",res.data.refreshToken);
-                let grantType = res.data.grantType.replace(res.data.grantType.charAt(0), res.data.grantType.charAt(0).toUpperCase());
-                console.log("grantType:", grantType);
-                this.$store.dispatch('saveToken', grantType+" "+res.data.accessToken, res.data.refreshToken)
-                this.$store.dispatch('saveName', res.data.name)
-                this.$store.dispatch('saveId', res.data.id)
-                this.$router.push({ path: '/'})
-                // const token = res.data.key
-                // dispatch('saveToken', token)
-                // dispaxtch('fetchCurrentUser')
-            
-            })
-            .catch(err =>{
-                console.log("에러")
-                console.log(err)
-            })
-      },
-      // event () {
-      //   this.$router.push({ path: '/' })
-      // }
-    },
-  }
-
+					this.$store.dispatch('saveName', res.data.name);
+					this.$store.dispatch('saveId', res.data.id);
+					this.$router.push({ path: '/' });
+					// const token = res.data.key
+					// dispatch('saveToken', token)
+					// dispaxtch('fetchCurrentUser')
+				})
+				.catch((err) => {
+					console.log('에러');
+					console.log(err);
+				});
+		},
+		// event () {
+		//   this.$router.push({ path: '/' })
+		// }
+	},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
