9e5d8cb9c0e22bc157dd1af695064247de4a70bd
오채명 <dhcoaud98@naver.com>
2022-08-15 20:42:22 +0900

    feat: 프로필 usersolving

--- frontend/src/components/Profile/SolvedProblemList.vue
+++ frontend/src/components/Profile/SolvedProblemList.vue
@@ -1,20 +1,27 @@
 <template>
   <v-container class="mt-5">
     <!-- v-for 사용하기 -->
-    <solved-problem-list-items></solved-problem-list-items>
+    <solved-problem-list-items
+      v-for="(userSolving, idx) in userSolvings"
+      :key="idx"
+      :userSolving="userSolving"
+    ></solved-problem-list-items>
     <br>  
-    <v-container class="text-center ">
-      <v-pagination
-        v-model="page"
-        :length="5"
-        color="#A384FF"
-      ></v-pagination>
-    </v-container>
+    <v-pagination
+      v-model="nowPage"
+      :length="endPage"
+      color="#A384FF"
+      circle
+      @input="handlePage()"
+    ></v-pagination>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+import drf from '@/api/drf'
 import SolvedProblemListItems from './SolvedProblemListItems.vue'
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'SolvedProblemList',
@@ -23,9 +30,60 @@ export default {
   },
   data () {
     return {
-      page: 1,
+      userSolvings: [],
+      nowPage: 1,
+      endPage: 2,
     }
   },
+  computed: {
+    ...mapGetters(['accessToken', 'currentUser'])
+  },
+  created() {
+    const params ={
+      page: 0,
+      size: 4,
+      sort: 'updated,DESC',
+    }
+    // 유저의 solving problem 조회
+    axios({
+      url: drf.api + 'solving',
+      method: 'get',
+      headers: {
+        Authorization : this.accessToken,
+      },
+      params: params
+    })
+    .then(res => {
+      console.log("userSolvings =", res.data.content)
+      this.userSolvings = res.data.content
+      this.endPage = res.data.totalPages
+    })
+  },
+  methods: {
+		handlePage() {
+			console.log('event = ', Number(event.target.ariaLabel.slice(-1)));
+			this.page = Number(event.target.ariaLabel.slice(-1));
+    
+      const params ={
+        page: this.page - 1,
+        size: 4,
+        sort: 'updated,DESC',
+      }
+      axios({
+        url: drf.api + 'solving',
+        method: 'get',
+        headers: {
+          Authorization : this.accessToken,
+        },
+        params: params
+      })
+      .then(res => {
+        console.log("userSolvings =",res.data.content)
+        this.userSolvings = res.data.content
+      })
+    }
+  }
+
 }
 </script>
 
